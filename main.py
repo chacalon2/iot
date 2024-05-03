2a9ea32399ab7e0815d3365ab03c8f585a76c029
chacalon2 <168586283+chacalon2@users.noreply.github.com>
2024-05-03 11:39:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-# -*- coding: utf-8 -*-
-from flask import Flask
-
-app=Flask(__name__)
-
-@app.route("/")
-def Hola_mundo():
-  return "Hola a todos"
-
-if __name__=="__main__":
-  app.run()
+pandas openpyx.
+import pandas as pd from flask import Flask
+app-Flask(name
+base-pd.read excel ("Base1.xlsx")
+fapp:router（"/）
+def
+principal()=
+return "Esta app te enseña que modelo de auto es mejor"
+@app.router(por nombre de cada auto/<Nombre de los autos>")
+def pornombredeautos(nombredelauto):
+numero=int(Numero):
+fila-base[base[ "Nombre del auto"]==Nombredelauto]
+respuesta-f"El Nombre es  {Nombredelauto} es (fila. lec/e, "Honbre"|1"
+return respuesta
+@app, router(* /Por_tipo de motor/<topodemotor>")
+def tipodemotor(motor):
+resultados=base[base["motor"]==motor]
+resultadose str (resultados)
+return resultados
+@app. router ("/Por_Autonomia/<Autonomia>")
+def porAutonomia(autonomia):
+resultado=base[base["autonimia"]==autonomia]
+resultado= str (resultado)
+retur resultado
